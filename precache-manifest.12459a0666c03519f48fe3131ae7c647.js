3964d302acfb18268a9febf282c8d8fee6d3aeca
Chen Qian <chenq2@illinois.edu>
2018-10-09 22:46:02 -0500

    Updates

---
+++ precache-manifest.12459a0666c03519f48fe3131ae7c647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e23ec7a00a6d22674c2",
+    "url": "/drunk-vision/static/js/runtime~main.4e23ec7a.js"
+  },
+  {
+    "revision": "695b464f71801ce37793",
+    "url": "/drunk-vision/static/js/main.695b464f.chunk.js"
+  },
+  {
+    "revision": "54111f25f16e89abea18",
+    "url": "/drunk-vision/static/js/1.54111f25.chunk.js"
+  },
+  {
+    "revision": "695b464f71801ce37793",
+    "url": "/drunk-vision/static/css/main.c1416da4.chunk.css"
+  },
+  {
+    "revision": "d0f4ca04615de242fca845d812305af2",
+    "url": "/drunk-vision/index.html"
+  }
+];
